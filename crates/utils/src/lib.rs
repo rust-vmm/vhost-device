e5a743d087e9e5e5652ed2be7356e3502b68696b
Erik Schilling <erik.schilling@linaro.org>
2023-06-13 13:29:42 +0200

    DRAFT: BROKEN: socket activation
    This still needs some work... Just posting as draft in order to
    contribute to the discussion on
    https://github.com/rust-vmm/vhost-device/pull/362
    
    Signed-off-by: Erik Schilling <erik.schilling@linaro.org>

--- crates/utils/src/lib.rs
+++ crates/utils/src/lib.rs
@@ -1,9 +1,50 @@
 use log::{info, warn};
-use std::{path::Path, sync::Arc};
+use std::{os::fd::FromRawFd, path::Path, sync::Arc};
 
 use vhost::vhost_user::{self, Listener};
 use vhost_user_backend::{VhostUserBackend, VhostUserDaemon, VringT};
 use vm_memory::{mmap::NewBitmap, GuestMemoryAtomic, GuestMemoryMmap};
+
+#[cfg(not(feature = "systemd"))]
+fn create_listener<P: AsRef<Path>>(socket_path: P) -> vhost_user::Result<Listener> {
+    Listener::new(socket_path, true)
+}
+
+#[cfg(feature = "systemd")]
+fn create_listener<P: AsRef<Path>>(socket_path: P) -> vhost_user::Result<Listener> {
+    use log::error;
+
+    let fds = systemd::daemon::listen_fds(false).unwrap();
+    let suitable_socket_fd = fds.iter().find(|&fd| {
+        let socket_path = Some(
+            socket_path
+                .as_ref()
+                .to_str()
+                .expect("socket_path should be valid utf-8 string"),
+        );
+
+        systemd::daemon::is_socket_unix(
+            fd,
+            Some(systemd::daemon::SocketType::Stream),
+            systemd::daemon::Listening::IsListening,
+            socket_path,
+        )
+        .unwrap()
+    });
+
+    if let Some(fd) = suitable_socket_fd {
+        unsafe { Ok(Listener::from_raw_fd(fd)) }
+    } else {
+        if !fds.is_empty() {
+            let socket_path = socket_path.as_ref().to_string_lossy();
+            error!(
+                "Detected socket activation, but did not find suitable \
+                socket for {socket_path}. Falling back to creating own socket..."
+            );
+        }
+        Listener::new(socket_path, true)
+    }
+}
 
 // Generics here are horrible...
 // Hopefully once https://github.com/rust-vmm/vhost/pull/155 lands,
@@ -28,7 +69,7 @@ impl<
     /// Lets the daemon start listening and waits until exit
     pub fn start<P: AsRef<Path>>(mut self, socket_path: P) -> vhost_user::Result<()> {
         self.daemon
-            .start(Listener::new(socket_path, true)?)
+            .start(create_listener(socket_path)?)
             .expect("Starting daemon");
 
         match self.daemon.wait() {

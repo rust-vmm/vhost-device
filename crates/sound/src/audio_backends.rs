ecf88fb60284edd0ac44c75c3c0c6cdd8712f6b1
Manos Pitsidianakis <manos.pitsidianakis@linaro.org>
2023-07-04 17:52:08 +0300

    sound: Add CTRL queue handler
    Signed-off-by: Emmanouil Pitsidianakis <manos.pitsidianakis@linaro.org>

--- crates/sound/src/audio_backends.rs
+++ crates/sound/src/audio_backends.rs
@@ -7,25 +7,50 @@ mod null;
 #[cfg(feature = "pw-backend")]
 mod pipewire;
 
+use std::sync::{Arc, RwLock};
+
 #[cfg(feature = "null-backend")]
 use self::null::NullBackend;
 #[cfg(feature = "pw-backend")]
 use self::pipewire::PwBackend;
-use crate::{Error, Result, SoundRequest};
+use crate::{device::ControlMessage, stream::Stream, Error, Result};
 
 pub trait AudioBackend {
-    fn write(&self, req: &SoundRequest) -> Result<()>;
+    fn write(&self, stream_id: u32) -> Result<()>;
 
-    fn read(&self, req: &mut SoundRequest) -> Result<()>;
+    fn read(&self, stream_id: u32) -> Result<()>;
+
+    fn set_parameters(&self, _stream_id: u32, _: ControlMessage) -> Result<()> {
+        Ok(())
+    }
+
+    fn prepare(&self, _stream_id: u32) -> Result<()> {
+        Ok(())
+    }
+
+    fn release(&self, _stream_id: u32, _: ControlMessage) -> Result<()> {
+        Ok(())
+    }
+
+    fn start(&self, _stream_id: u32) -> Result<()> {
+        Ok(())
+    }
+
+    fn stop(&self, _stream_id: u32) -> Result<()> {
+        Ok(())
+    }
 }
 
-pub fn alloc_audio_backend(name: String) -> Result<Box<dyn AudioBackend + Send + Sync>> {
+pub fn alloc_audio_backend(
+    name: String,
+    streams: Arc<RwLock<Vec<Stream>>>,
+) -> Result<Box<dyn AudioBackend + Send + Sync>> {
     log::trace!("allocating audio backend {}", name);
     match name.as_str() {
         #[cfg(feature = "null-backend")]
-        "null" => Ok(Box::new(NullBackend::new())),
+        "null" => Ok(Box::new(NullBackend::new(streams))),
         #[cfg(feature = "pw-backend")]
-        "pipewire" => Ok(Box::new(PwBackend::new())),
+        "pipewire" => Ok(Box::new(PwBackend::new(streams))),
         _ => Err(Error::AudioBackendNotSupported),
     }
 }

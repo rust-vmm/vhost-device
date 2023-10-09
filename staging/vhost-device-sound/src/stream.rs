06c3f47f6ce97416f14981d39fec029c6cfadf9c
Manos Pitsidianakis <manos.pitsidianakis@linaro.org>
2023-10-09 15:27:30 +0300

    sound: Add Direction enum
    We use input/output direction values in many places, yet we lose Rust's
    exhaustive static checks when matching for a stream's direction. This
    commits adds a Direction enum and From/Into implementations for audio
    backend respective direction types.
    
    Signed-off-by: Manos Pitsidianakis <manos.pitsidianakis@linaro.org>

--- staging/vhost-device-sound/src/stream.rs
+++ staging/vhost-device-sound/src/stream.rs
@@ -6,7 +6,7 @@ use std::{collections::VecDeque, sync::Arc};
 use thiserror::Error as ThisError;
 use vm_memory::{Address, Bytes, Le32, Le64};
 
-use crate::{virtio_sound::*, IOMessage, SUPPORTED_FORMATS, SUPPORTED_RATES};
+use crate::{virtio_sound::*, Direction, IOMessage, SUPPORTED_FORMATS, SUPPORTED_RATES};
 
 /// Stream errors.
 #[derive(Debug, ThisError, PartialEq)]
@@ -172,7 +172,7 @@ pub struct Stream {
     pub params: PcmParams,
     pub formats: Le64,
     pub rates: Le64,
-    pub direction: u8,
+    pub direction: Direction,
     pub channels_min: u8,
     pub channels_max: u8,
     pub state: PCMState,
@@ -183,7 +183,7 @@ impl Default for Stream {
     fn default() -> Self {
         Self {
             id: 0,
-            direction: VIRTIO_SND_D_OUTPUT,
+            direction: Direction::Output,
             formats: SUPPORTED_FORMATS.into(),
             rates: SUPPORTED_RATES.into(),
             params: PcmParams::default(),
